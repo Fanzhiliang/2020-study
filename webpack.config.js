027ff95a93f7f4a3b4db297fe72184d72476b86a
fanzhiliang <505911050@qq.com>
2020-12-31 11:13:40 +0800

    feat(babel)

--- webpack.config.js
+++ webpack.config.js
@@ -36,6 +36,12 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.(js|jsx)$/,
+        use: 'babel-loader',
+        exclude: path.resolve('node_modules'),
+        include: path.resolve('src')
+      },
+      {
         test: /\.css$/,
         // 注意 loader 加载顺序和书写顺序是相反的
         use: [
